a8974ab8ad51a8d82e5e6a510315124000b3f766
Ying Zhong <0x00eeee@gmail.com>
2026-03-03 14:19:27 +0800

    Improve code copy for mermaid blocks (#60)

--- src/render.ts
+++ src/render.ts
@@ -158,17 +158,21 @@ const pluginsReady = Promise.all(pluginInits).then(() => {
   // Mermaid, for full builds only
   if (__FULL_BUILD__) {
     const renderFence = mdit.renderer.rules.fence;
-    mdit.renderer.rules.fence = (tokens, idx, options, env, slf) => {
+    mdit.renderer.rules.fence = (tokens, idx, options, env, self) => {
       const token = tokens[idx];
       const code = token.content.trim();
       const lang = token.info.trim();
 
+      token.attrSet('data-code', code + '\n');
+      const dataCode = self.renderAttrs(token);
+      const escapedCode = mdit.utils.escapeHtml(code);
+
       if (lang === 'mermaid') {
-        return `<div class="mermaid">${code}</div>`;
+        return `<div class="mermaid"${dataCode}>${escapedCode}</div>`;
       }
 
       if (renderFence !== undefined) {
-        return renderFence(tokens, idx, options, env, slf);
+        return renderFence(tokens, idx, options, env, self);
       }
 
       return `<pre><code class="language-${lang}">${mdit.utils.escapeHtml(code)}</code></pre>`;
@@ -183,7 +187,7 @@ const pluginsReady = Promise.all(pluginInits).then(() => {
       return `
       <div class="code-copy-wrapper" onmouseenter="this.querySelector('.code-copy-button').style.opacity='1'" onmouseleave="this.querySelector('.code-copy-button').style.opacity='0'">
         ${codeBlock}
-        <button title="${localized('copyCode')}" aria-label="${localized('copyCode')}" class="code-copy-button" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.opacity='0'">
+        <button title="${localized('copyCode')}" aria-label="${localized('copyCode')}" class="code-copy-button" onclick="navigator.clipboard.writeText(this.previousElementSibling.dataset.code ?? this.previousElementSibling.innerText); this.style.opacity='0'">
           <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16">
             <path fill="currentColor" d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path>
             <path fill="currentColor" d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
